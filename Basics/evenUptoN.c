a25b6858c6ad3dd92c686106ab48d2ba56ed4b0e
Anchal Jaiswal <jaiswalanchal9648@gmail.com>
2026-08-04 18:31:33 +0530

    add even number upto N

---
+++ Basics/evenUptoN.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Even numbers from 1 to %d are:\n", n);
+
+    for (i = 2; i <= n; i += 2) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
